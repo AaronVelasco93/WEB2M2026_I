9660ae8e4eae009a69c649dce646786e85364072
AaronVelasco93 <aaronvelasco1993@outlook.com>
2025-08-28 10:32:14 -0600

    Clase del 28082025

---
+++ 28082025/app4.js
@@ -0,0 +1,22 @@
+/*
+let numUsuario = parseInt(prompt("Ingresa un numero mayor a 10"));
+
+if(numUsuario >= 10){
+    console.log("Genial")
+}else{
+    console.log("Mal");
+}
+    */
+
+
+// let port ;
+
+// if(port){
+//     console.log("Deficion de puerto correcto");
+// }else{
+//     console.error("Error en puerto");
+// }
+
+
+let datos = 15 != 15;
+console.log(datos);
