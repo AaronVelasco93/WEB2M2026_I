ba1376f4cd51b13b0f4432ecf57c0bd2bd3975d5
AaronVelasco93 <aaronvelasco1993@outlook.com>
2025-09-04 10:38:59 -0600

    Clase "04092025"

---
+++ 04092025/app5.js
@@ -0,0 +1,32 @@
+let nommbre = "aaron";
+/*
+    console.log("Tu nombre es : "+ nommbre);
+
+    console.log(`Tu nombres es ${nommbre}`);
+
+    console.log(`
+    Hola como estas : 
+    ${nommbre} 
+
+        `);
+
+
+*/
+
+// console.log(`Hola como estas: ${nommbre.toLocaleUpperCase()}`);
+
+
+var nombreUsuario = "HuronMarron";
+let estado = true;
+console.log(`${estado ? 'Todo bien ':'Adios '} ${nombreUsuario} `);
+
+
+var nombre = "Aaron";
+let bandera = true;
+
+
+if(bandera){
+    console.log("Todo bien "+nombre);
+}else{
+    console.log("Adios "+nombre);
+}
